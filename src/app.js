cb1d3fca33ae6efd677ceaaa2748fe92eadd06dd
HugoMProject <hugomoyano1996@gmail.com>
2022-09-16 14:59:52 -0300

    se han movido los archivos de imagenes, y creado la carpeta estatica public, ademas de hacer dinamica la pagina, y empezado a dar estilos, faltal poder renderisar las imagenes que no se estas mostrando, que se encuentran en la carperta public

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views' , __dirname, '/views');
 
+app.use(express.static('public'));
+app.use('/desktop',express.static(__dirname +'public'));
+
 app.use(require('./routes/user'));
 
 app.listen(3000,() => console.log('servidor iniciado en el puerto 3000'))
