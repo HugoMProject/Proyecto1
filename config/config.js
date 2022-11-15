a7c22b0f7b6e4f3273fe8fd1ed55a6c6cc1963af
HugoMProject <hugomoyano1996@gmail.com>
2022-11-15 14:06:12 -0300

    se ah editado los archivos models/index.js y config/config.js en es te esltimo se ha cambiado la extencion de json a js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,17 @@
-{
+module.exports = {
+
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "root",
+  DB: "e_commerce1",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  }, 
+  // modelo por defencto de sequelize
   "development": {
     "username": "root",
     "password": "root",
