c6b4521c09cdf1fec374809148dfc708d80ff789
HugoMProject <hugomoyano1996@gmail.com>
2022-11-24 18:19:34 -0300

    se ha creado la carpeta de scripts y los correspondientes archivos js addCart , upDateCart y removeCart

--- config/config.js
+++ config/config.js
@@ -11,26 +11,26 @@ module.exports = {
     acquire: 30000,
     idle: 10000,
   }, 
-  // modelo por defencto de sequelize
-  "development": {
-    "username": "root",
-    "password": "root",
-    "database": "e_commerce1",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+  //                      modelo por defencto de sequelize
+  // "development": {
+  //   "username": "root",
+  //   "password": "root",
+  //   "database": "e_commerce1",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // },
+  // "test": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_test",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // },
+  // "production": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_production",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // }
 }
