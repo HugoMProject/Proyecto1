776ee3c92e9a29d08cd749b1f2022346f5da702c
HugoMProject <hugomoyano1996@gmail.com>
2023-01-27 21:51:01 -0300

    se modifico el archivo config.js para agregar el puerto

--- models/index.js
+++ models/index.js
@@ -36,6 +36,7 @@ const dbConfig = require("../config/config.js");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
+  port: dbConfig.PORT,
   dialect: dbConfig.dialect,
   operatorsAliases: false,
 
